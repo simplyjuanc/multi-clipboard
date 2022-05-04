a84c4c0b7bfe9ca6695ce65d6367f6467c44429c
simplyjuanc_lr <juan.vasquez@liveramp.com>
2022-05-04 21:56:04 +0100

    Define basic save func and named other required func

--- main.py
+++ main.py
@@ -2,14 +2,36 @@ import sys
 import clipboard
 import json
 
-data = clipboard.paste()
+
+def save_item(cache, keyword, item):
+    with open(cache, 'a') as clipb:
+        json.dump({keyword : item}, clipb)
+
+
+def load_item(cache, keyword, item):
+    pass
+
+def del_item(cache, keyword, item):
+    pass
+
+
 
 
 if len(sys.argv) != 2:
     print('wrong argv length')
 
+
+
+
+data = clipboard.paste()
+CACHE = "PATH_SOMEWHERE"
+KEYWORD = sys.argv[1]
+
+
 command = input("What would you like to do? (save/list/load/del)\n")
 if command == "list":
+    save_item(cache=CACHE, keyword=KEYWORD, item=data)
+    
     print('list')
 elif command == "save":
     print('save')
